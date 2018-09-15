038679bfdd5f2aaebdb8edb618d99fe07d57f6ba
Haibo Gu <ghbhaha@users.noreply.github.com>
2018-09-15 16:55:43 +0800

    Call location
    Change-Id: Ic7e7a6d10a247e3eb47ca2c395389dbde9e5bc22

--- config/common.mk
+++ config/common.mk
@@ -61,6 +61,14 @@ endif
 PRODUCT_COPY_FILES += \
     vendor/arrow/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
+# Phonelocation!
+PRODUCT_COPY_FILES +=  \
+    vendor/arrow/prebuilt/common/media/location/suda-phonelocation.dat:system/media/location/suda-phonelocation.dat
+
+ # World SPN overrides list
+PRODUCT_COPY_FILES += \
+    vendor/arrow/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
+
 # Sensitive Phone Numbers list
 PRODUCT_COPY_FILES += \
     vendor/arrow/prebuilt/common/etc/sensitive_pn.xml:system/etc/sensitive_pn.xml
--- config/packages.mk
+++ config/packages.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# LocationProvider
+PRODUCT_PACKAGES += \
+    PhoneLocationProvider
 
 # Required packages
 PRODUCT_PACKAGES += \
